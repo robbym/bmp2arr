3094f66b9bd0d79055af25c5562581bd825ebdb1
Robby Madruga <rmadruga@illumtechnology.com>
2016-06-24 16:08:18 -0700

    Changed output representation to 6bit packed RGB.

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("bmp2arr")
-                      .version("1.0")
-                      .author("Robby Madruga <robbymadruga@gmail.com>")
-                      .about("Converts a monochrome bmp file to a bitmapped C array")
-                      .arg(Arg::with_name("LINE_COUNT")
-                               .short("c")
-                               .takes_value(true)
-                               .help("Specifies the number of bytes per row"))
-                      .arg(Arg::with_name("FILE")
-                               .help("Sets the bmp file to convert")
-                               .required(true))
-                      .get_matches();
+        .version("1.0")
+        .author("Robby Madruga <robbymadruga@gmail.com>")
+        .about("Converts a monochrome bmp file to a bitmapped C array")
+        .arg(Arg::with_name("LINE_COUNT")
+            .short("c")
+            .takes_value(true)
+            .help("Specifies the number of bytes per row"))
+        .arg(Arg::with_name("FILE")
+            .help("Sets the bmp file to convert")
+            .required(true))
+        .get_matches();
 
     let bmpfile = matches.value_of("FILE").unwrap();
     let line_wrap = matches.value_of("LINE_COUNT").unwrap_or("16").parse::<usize>().unwrap();
@@ -25,14 +25,22 @@ fn main() {
     println!("#define image_width {}", w);
     println!("#define image_height {}", h);
 
-    let mut v = Vec::new();
-
-    for y in 0..h {
-        for x in 0..w {
-            let p = bmp.get_pixel(x, y);
+    let v = (0..w * h)
+        .map(|i| bmp.get_pixel(i % w, i / w))
+        .fold(Vec::new(), |mut v, p| {
             v.push(p.r);
             v.push(p.g);
             v.push(p.b);
+            v
+        });
+
+    let mut ps = Vec::<u8>::new();
+
+    for i in 0..w * h * 3 {
+        if ps.len() > i as usize {
+            ps[(i / 4) as usize] |= (v[i as usize] >> 6) << (2 * (i % 4));
+        } else {
+            ps.push(v[i as usize] >> 6);
         }
     }
 
